fbabf93a497b54db90f2fed9286b62e90e490a87
NotChristian <raphaelheryanto270605@gmail.com>
2024-09-04 16:17:09 +0700

    update user

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306152323',
+        'name': 'Christian Raphael Heryanto',
+        'class': 'PBP D'
     }
 
     return render(request, "main.html", context)
