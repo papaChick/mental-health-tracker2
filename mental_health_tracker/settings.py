fda40c3fa50c462388b12569361f9737a772f651
NotChristian <raphaelheryanto270605@gmail.com>
2024-09-11 15:38:42 +0700

    tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l(osk1l1jy7$a66@(7pz6mlfc=677$2)=e6c(z=8p@are8ww_8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "christian-raphael-mentalhealthtracker2.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "christian-raphael-mentalhealthtracker3.pbp.cs.ui.ac.id"]
 
 
 # Application definition
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://christian-raphael-mentalhealthtracker2.pbp.cs.ui.ac.id", "https://christian-raphael-mentalhealthtracker2.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://christian-raphael-mentalhealthtracker3.pbp.cs.ui.ac.id", "https://christian-raphael-mentalhealthtracker3.pbp.cs.ui.ac.id"]
